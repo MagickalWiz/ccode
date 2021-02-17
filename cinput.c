399b0f32365cd645d0cab6139586855780b5d8a3
MagickalWiz <54958629+MagickalWiz@users.noreply.github.com>
2021-02-17 10:56:09 -0800

    Add files via upload

---
+++ cinput.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+	char str [16];
+	int i;
+	printf("Enter a number: ");
+	scanf("%15s",str);
+	printf("Enter a hexadecimal number: ");
+	scanf("%x",&i);
+	printf("Hexadecimal %#x ( %d). \n",i,i);
+	return 0
