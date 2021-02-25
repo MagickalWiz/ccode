fcf80b0aa1caf15359925d77dd8b52d200b3779a
MagickalWiz <54958629+MagickalWiz@users.noreply.github.com>
2021-02-25 13:46:37 -0800

    Add files via upload

---
+++ initials.c
@@ -0,0 +1,21 @@
+//gcc initials.c -o intials.o
+#include <stdio.h>
+
+int main(){
+	printf("      /\\            /\\\n");
+	printf("     //\\\\          //\\\\\n");
+	printf("    //  \\\\        //  \\\\\n");
+	printf("   //    \\\\      //    \\\\\n");
+	printf("  //      \\\\    //      \\\\\n");
+	printf(" //        \\\\  //        \\\\\n");
+	printf("//          \\\\//          \\\\\n");
+	printf("----------------------------\n");
+	printf("\\\\          //\\\\          //\n");
+	printf(" \\\\        //  \\\\        //\n");
+	printf("  \\\\      //    \\\\      //\n");
+	printf("   \\\\    //      \\\\    //\n");
+	printf("    \\\\  //        \\\\  //\n");
+	printf("     \\\\//          \\\\//\n");
+	printf("      \\/            \\/\n");
+	return 0;
+}
