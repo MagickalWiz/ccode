d5b196bda755606119bde28b6687580f75010c8b
MagickalWiz <54958629+MagickalWiz@users.noreply.github.com>
2021-03-09 11:45:24 -0800

    Add files via upload

---
+++ cvariables.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int i;
+	double d;
+	float f;
+	char c;
+	string s;
+	bool done;
+	
+	i = 123456789;
+	d = 3.14159268;
+	f = 1.23;
+	c = 'A';
+	s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	done = true;
+	
+	cout << i <<"\n";
+	cout << d <<"\n";
+	cout << f <<"\n";
+	cout << c <<"\n";
+	cout << s <<"\n";
+	cout << done <<"\n";
+	
+	done = false;
+	
+	cout << done <<"\n";
+}
